215880d911318ac004c939ba44c1798c08442c9f
stephen <smplaceway@oakland.edu>
2022-12-13 13:29:50 -0500

    initial

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import { StatusBar } from "expo-status-bar";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	SafeAreaView,
+	Alert,
+	Image,
+	ScrollView,
+} from "react-native";
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	seperator: {
+		marginVertical: 8,
+		borderBottomColor: "silver",
+		borderBottomWidth: StyleSheet.hairlineWidth,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		textAlign: "center",
+		marginVertical: 10,
+		color: "blue",
+		fontFamily: "Futura",
+	},
+	fixToText: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	image: {
+		justifyContent: "space-between",
+		resizeMode: "contain",
+		height: 300,
+		width: 430,
+	},
+	infobutton: {
+		fontFamily: "Futura",
+	},
 });
+const Seperator = () => <View style={styles.seperator} />;
+
+const App = () => (
+	<SafeAreaView Style={styles.container}>
+		<ScrollView>
+			<View>
+				<Image style={styles.image} source={require("./img/avalanche.webp")} />
+				<Text style={styles.title}>2005 Chevy Avalanche</Text>
+				<Button
+					style={styles.infobutton}
+					title="View information"
+					onPress={() => Alert.alert("button pressed")}
+				/>
+			</View>
+			<Seperator />
+			<Seperator />
+			<View>
+				<Image
+					style={styles.image}
+					source={require("./img/1984corvette.jpeg")}
+				/>
+				<Text style={styles.title}>1984 Chevy Corvette</Text>
+				<Button
+					style={styles.infobutton}
+					title="View information"
+					onPress={() => Alert.alert("button pressed")}
+				/>
+			</View>
+			<Seperator />
+			<Seperator />
+			<View>
+				<Image style={styles.image} source={require("./img/Aura.jpeg")} />
+				<Text style={styles.title}>2008 Saturn Aura</Text>
+				<Button
+					style={styles.infobutton}
+					title="View information"
+					onPress={() => Alert.alert("button pressed")}
+				/>
+			</View>
+		</ScrollView>
+	</SafeAreaView>
+);
+// const styles = StyleSheet.create({
+// 	Container: {
+// 		flex: 1,
+// 		justifyContent: "center",
+// 		marginHorizontal: 16,
+// 	},
+// });
+export default App;
